c7a359efd94f2cc70094180ad4597fcaadd9b573
jxnu-liguobin <568845948@qq.com>
2018-05-28 11:00:00 +0800

    SpringBoot整合swagger自动化测试

---
+++ swaggerTest/src/main/java/cn/edu/jxnu/swaggerTest/SwaggerTestApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.jxnu.swaggerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class SwaggerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerTestApplication.class, args);
+	}
+}
